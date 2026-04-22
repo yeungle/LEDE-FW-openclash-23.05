a5963830c564c68c981161576545e4439cdf45c7
yeungle <75871729+yeungle@users.noreply.github.com>
2026-04-22 11:04:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -82,7 +82,10 @@ cp -rf extra-ipk/op-homebox/luci-app-homebox package/luci-app-homebox
 rm -rf extra-ipk
 
 git clone --depth=1 https://github.com/jjm2473/luci-app-autotimeset.git package/luci-app-autotimeset
-git clone --depth=1 https://github.com/coolsnowwolf/luci/openwrt-23.05/applications/luci-app-accesscontrol.git package/luci-app-accesscontrol
+
+git clone --depth=1 -b openwrt-23.05 https://github.com/coolsnowwolf/luci.git package/luci
+cp -rf package/luci/applications/kenzok8-packages/luci-app-accesscontrol package/luci-app-accesscontrol
+rm -rf package/luci
 
 #./scripts/feeds update -a
 #./scripts/feeds install -a
